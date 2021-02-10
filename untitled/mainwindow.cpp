c15ae23e41c0cff466fcb480f9e437db3390c70b
ivanos121 <loggie121@gmail.com>
2021-02-10 18:55:56 +0300

    write confioguration

--- untitled/mainwindow.cpp
+++ untitled/mainwindow.cpp
@@ -747,10 +747,61 @@ void MainWindow::on_pushButton_7_clicked()
           ui->comboBox->addItem(info.portName());
           ui->comboBox->addItem("ttyMP0");
           ui->comboBox->addItem("ttyMP1");
+          ui->comboBox->addItem("ttyDUMMY");
           ui->comboBox->setCurrentIndex(1);
         }
 }
 
+
+uint16_t CRC16 (QByteArray nData, int wLength)
+{
+static const uint16_t wCRCTable[] = {
+   0X0000, 0XC0C1, 0XC181, 0X0140, 0XC301, 0X03C0, 0X0280, 0XC241,
+   0XC601, 0X06C0, 0X0780, 0XC741, 0X0500, 0XC5C1, 0XC481, 0X0440,
+   0XCC01, 0X0CC0, 0X0D80, 0XCD41, 0X0F00, 0XCFC1, 0XCE81, 0X0E40,
+   0X0A00, 0XCAC1, 0XCB81, 0X0B40, 0XC901, 0X09C0, 0X0880, 0XC841,
+   0XD801, 0X18C0, 0X1980, 0XD941, 0X1B00, 0XDBC1, 0XDA81, 0X1A40,
+   0X1E00, 0XDEC1, 0XDF81, 0X1F40, 0XDD01, 0X1DC0, 0X1C80, 0XDC41,
+   0X1400, 0XD4C1, 0XD581, 0X1540, 0XD701, 0X17C0, 0X1680, 0XD641,
+   0XD201, 0X12C0, 0X1380, 0XD341, 0X1100, 0XD1C1, 0XD081, 0X1040,
+   0XF001, 0X30C0, 0X3180, 0XF141, 0X3300, 0XF3C1, 0XF281, 0X3240,
+   0X3600, 0XF6C1, 0XF781, 0X3740, 0XF501, 0X35C0, 0X3480, 0XF441,
+   0X3C00, 0XFCC1, 0XFD81, 0X3D40, 0XFF01, 0X3FC0, 0X3E80, 0XFE41,
+   0XFA01, 0X3AC0, 0X3B80, 0XFB41, 0X3900, 0XF9C1, 0XF881, 0X3840,
+   0X2800, 0XE8C1, 0XE981, 0X2940, 0XEB01, 0X2BC0, 0X2A80, 0XEA41,
+   0XEE01, 0X2EC0, 0X2F80, 0XEF41, 0X2D00, 0XEDC1, 0XEC81, 0X2C40,
+   0XE401, 0X24C0, 0X2580, 0XE541, 0X2700, 0XE7C1, 0XE681, 0X2640,
+   0X2200, 0XE2C1, 0XE381, 0X2340, 0XE101, 0X21C0, 0X2080, 0XE041,
+   0XA001, 0X60C0, 0X6180, 0XA141, 0X6300, 0XA3C1, 0XA281, 0X6240,
+   0X6600, 0XA6C1, 0XA781, 0X6740, 0XA501, 0X65C0, 0X6480, 0XA441,
+   0X6C00, 0XACC1, 0XAD81, 0X6D40, 0XAF01, 0X6FC0, 0X6E80, 0XAE41,
+   0XAA01, 0X6AC0, 0X6B80, 0XAB41, 0X6900, 0XA9C1, 0XA881, 0X6840,
+   0X7800, 0XB8C1, 0XB981, 0X7940, 0XBB01, 0X7BC0, 0X7A80, 0XBA41,
+   0XBE01, 0X7EC0, 0X7F80, 0XBF41, 0X7D00, 0XBDC1, 0XBC81, 0X7C40,
+   0XB401, 0X74C0, 0X7580, 0XB541, 0X7700, 0XB7C1, 0XB681, 0X7640,
+   0X7200, 0XB2C1, 0XB381, 0X7340, 0XB101, 0X71C0, 0X7080, 0XB041,
+   0X5000, 0X90C1, 0X9181, 0X5140, 0X9301, 0X53C0, 0X5280, 0X9241,
+   0X9601, 0X56C0, 0X5780, 0X9741, 0X5500, 0X95C1, 0X9481, 0X5440,
+   0X9C01, 0X5CC0, 0X5D80, 0X9D41, 0X5F00, 0X9FC1, 0X9E81, 0X5E40,
+   0X5A00, 0X9AC1, 0X9B81, 0X5B40, 0X9901, 0X59C0, 0X5880, 0X9841,
+   0X8801, 0X48C0, 0X4980, 0X8941, 0X4B00, 0X8BC1, 0X8A81, 0X4A40,
+   0X4E00, 0X8EC1, 0X8F81, 0X4F40, 0X8D01, 0X4DC0, 0X4C80, 0X8C41,
+   0X4400, 0X84C1, 0X8581, 0X4540, 0X8701, 0X47C0, 0X4680, 0X8641,
+   0X8201, 0X42C0, 0X4380, 0X8341, 0X4100, 0X81C1, 0X8081, 0X4040 };
+
+uint8_t nTemp;
+uint16_t wCRCWord = 0xFFFF;
+int index = 0;
+
+   while (wLength--)
+   {
+      nTemp = nData[index] ^ wCRCWord;
+      wCRCWord >>= 8;
+      wCRCWord  ^= wCRCTable[nTemp];
+      index++;
+   }
+   return wCRCWord;
+}
 
 uint CRC16_2(QByteArray buf, int len)
 {
@@ -830,7 +881,7 @@ void MainWindow::timerTimeout()
         buf[4]=0x00; //старший байт количества регистров для чтения
         buf[5]=0x78; //младший байт количества регистров для чтения
         crc = CRC16_2(buf, 6); //считаем сумму - считаем сумму по заполненому
-        qDebug() << crc;
+        //qDebug() << crc;
         buf[6]=crc & 0x00FF; //сташий байт crc
         buf[7]=(crc & 0xFF00) >> 8; //младший байт crc
         buf[8]=0x00;
@@ -839,7 +890,7 @@ void MainWindow::timerTimeout()
         port->write(buf, buf.length());
         port->waitForBytesWritten(100);
         port->flush();
-        qDebug() << "Tx: "<< buf.toHex();
+        //qDebug() << "Tx: "<< buf.toHex();
         int answerLength = 0;
         QByteArray answer;
         do
@@ -855,7 +906,7 @@ void MainWindow::timerTimeout()
             if (answerLength == 0)
             {
                 answerLength = (uint8_t)ansBuf[2] - ansBuf.length() + 3 + 2;
-                qDebug() << "total: " << (uint8_t)ansBuf[2];
+                //qDebug() << "total: " << (uint8_t)ansBuf[2];
                 answer.append(ansBuf.remove(0,3));
             }
             else
@@ -876,7 +927,7 @@ void MainWindow::timerTimeout()
         buf[4]=0x00; //старший байт количества регистров для чтения
         buf[5]=0x48; //младший байт количества регистров для чтения
         crc = CRC16_2(buf, 6); //считаем сумму - считаем сумму по заполненому
-        qDebug() << crc;
+        //qDebug() << crc;
         buf[6]=crc & 0x00FF; //сташий байт crc
         buf[7]=(crc & 0xFF00) >> 8; //младший байт crc
         buf[8]=0x00;
@@ -884,7 +935,7 @@ void MainWindow::timerTimeout()
         port->write(buf, buf.length());
         port->waitForBytesWritten(100);
         port->flush();
-        qDebug() << "Tx: "<< buf.toHex();
+        //qDebug() << "Tx: "<< buf.toHex();
         answerLength = 0;
 
         do
@@ -900,7 +951,7 @@ void MainWindow::timerTimeout()
             if (answerLength == 0)
             {
                 answerLength = (uint8_t)ansBuf[2] - ansBuf.length() + 3 + 2;
-                qDebug() << "total: " << (uint8_t)ansBuf[2];
+                //qDebug() << "total: " << (uint8_t)ansBuf[2];
                 answer.append(ansBuf.remove(0,3));
             }
             else
@@ -912,8 +963,8 @@ void MainWindow::timerTimeout()
         answer_crc = answer[answer.length() - 1] << 8 + answer[answer.length() - 2];
         answer.remove(384, answer.length() - 1);
 
-        qDebug() << "reseived bytes: " << answer.length();
-        qDebug() << "Rx: " << answer.toHex();
+        //qDebug() << "reseived bytes: " << answer.length();
+        //qDebug() << "Rx: " << answer.toHex();
         port->close();
 
         QVector<ArchiverChannel> archiverChannels(64);
@@ -947,7 +998,7 @@ void MainWindow::timerTimeout()
                 {
                     int k=ui->tableView->model()->data(ui->tableView->model()->index(i, 8) ).toInt();
                     uint32_t rawBEValue = archiverChannels[i].rawValue;
-                    qDebug() << i << "=" << archiverChannels[i].rawValue;
+                    //qDebug() << i << "=" << archiverChannels[i].rawValue;
 
                     QString value = ui->tableView->model()->data(ui->tableView->model()->index(i, 7) ).toString();
 
@@ -1214,10 +1265,15 @@ void MainWindow::setDisabledCells()
 
 void MainWindow::on_action_triggered()
 {
+    QSerialPort* port = openArchiverPort();
+    port->flush();
+
     for (int i = 0; i < 64; i++)
     {
+        qDebug() << "Begin channel " << i;
         QByteArray buf;
         buf.resize(31 * 2 + 7 + 2);
+        buf.fill(0);
         buf[0] = 0x38;
         buf[1] = 0x10;
 
@@ -1232,7 +1288,7 @@ void MainWindow::on_action_triggered()
         buf[6] = 62;
 
         buf[7] = 0;
-        buf[8] = ui->tableView->model()->data(ui->tableView->model()->index(i, 2)).toInt() + (ui->tableView->model()->data(ui->tableView->model()->index(i, 2)).toInt() << 1);
+        buf[8] = ui->tableView->model()->data(ui->tableView->model()->index(i, 2)).toInt() + ((ui->tableView->model()->data(ui->tableView->model()->index(i, 2)).toInt() << 1));
 
         QString channelName = ui->tableView->model()->data(ui->tableView->model()->index(i, 3) ).toString();
         channelName.truncate(31);
@@ -1242,7 +1298,7 @@ void MainWindow::on_action_triggered()
 
         for (int pos = 0; pos < channelNameBuffer.size(); pos++)
         {
-            buf[8 + pos] = channelNameBuffer[pos];
+            buf[9 + pos] = channelNameBuffer[pos];
         }
 
         buf[40] = 0;
@@ -1332,9 +1388,16 @@ void MainWindow::on_action_triggered()
         buf[56] = (errorarchiveThreshold.rawValue & 0x000000FF);
 
         buf[57] = 0;
-        buf[58] = ui->tableView->model()->data(ui->tableView->model()->index(i, 11) ).toInt();
+        if (ui->tableView->model()->data(ui->tableView->model()->index(i, 11) ).toInt() == 3)
+        {
+            buf[58] = 0;
+        } else if (ui->tableView->model()->data(ui->tableView->model()->index(i, 11) ).toInt() == 4)
+        {
+            buf[58] = 1;
+        }
 
-        uint16_t registerAddress = ui->tableView->model()->data(ui->tableView->model()->index(i, 12) ).toInt();
+        QString resgisterAddressString = ui->tableView->model()->data(ui->tableView->model()->index(i, 12) ).toString();
+        uint16_t registerAddress = resgisterAddressString.toInt(nullptr, 16);
         buf[59] = (uint8_t)((registerAddress & 0xFF00) >> 8);
         buf[60] = (uint8_t)(registerAddress & 0x00FF);
 
@@ -1363,19 +1426,59 @@ void MainWindow::on_action_triggered()
         buf[67] = (uint8_t)((index & 0xFF00) >> 8);
         buf[68] = (uint8_t)(index & 0x00FF);
 
-        uint16_t crc = CRC16_2(buf, buf.size() - 2);
+
+        /*uint8_t package[] = { 0x38, 0x10, 0x01, 0x00, 0x00, 0x1F, 0x3E, 0x00, 0x02, 0x50, 0x55, 0x5F, 0x72, 0x69, 0x67, 0x68,
+        0x74, 0x5F, 0x66, 0x72, 0x6F, 0x6E, 0x74, 0x31, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x01, 0xF4, 0x00,
+        0x0A, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x10, 0x09, 0x00, 0x01, 0x00,
+        0x00, 0x00, 0x00, 0xFF, 0xFF};*/
+
+        /*for (int kkk = 0; kkk < 69; kkk++)
+        {
+            buf[kkk] = package[kkk];
+        }*/
+
+        uint16_t crc = CRC16(buf, buf.size() - 2);
 
         buf[69]=crc & 0x00FF;
         buf[70]=(crc & 0xFF00) >> 8;
 
-        QSerialPort* port = openArchiverPort();
+        //QString debug_crc = QString("%1").arg(crc, 0, 16);
+        //qDebug() << "crc: " << debug_crc;
+
         port->write(buf, buf.length());
         port->waitForBytesWritten(1000);
         port->flush();
-        delete port;
+
+        int answerLength = 0;
+        QByteArray answer;
+        do
+        {
+            if (!port->waitForReadyRead(100))
+            {
+                break;
+            }
+            QByteArray ansBuf = port->readAll();
+
+            if (answerLength == 0)
+            {
+                answerLength = (uint8_t)ansBuf[2] - ansBuf.length() + 3 + 2;
+                //qDebug() << "total: " << (uint8_t)ansBuf[2];
+                answer.append(ansBuf.remove(0,3));
+            }
+            else
+            {
+                answerLength -= ansBuf.length();
+                answer.append(ansBuf);
+            }
+        } while (answerLength != 0);
 
          QThread::msleep(100);
+         qDebug() << "End channel " << i;
     }
+
+    port->close();
+    delete port;
 
     QMessageBox::information(this, "Завершено", "Запись данных завершена!");
 }
